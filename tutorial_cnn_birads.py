6449afd82b91387ece27575dc683aae43a64b994
phlc <pedrocarvalho@outlook.com>
2022-06-11 20:52:42 -0300

    end tests

--- tutorial_cnn_birads.py
+++ tutorial_cnn_birads.py
@@ -23,19 +23,28 @@ classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adicionando a Segunda Camada de Convolução
-# classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 
-# classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
 
 # Passo 3 - Flattening
 classifier.add(Flatten())
 
 # Passo 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units = 4, activation = 'softmax'))
 
 # Compilando a rede
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
 # Criando os objetos train_datagen e validation_datagen com as regras de pré-processamento das imagens
 train_datagen = ImageDataGenerator(rescale = 1./255,
@@ -47,53 +56,21 @@ validation_datagen = ImageDataGenerator(rescale = 1./255)
 
 
 # Pré-processamento das imagens de treino e validação
-# train_path = 'dogs_cats.nosync/dataset_train'
-train_path = 'homer_bart.nosync/training_set'
-training_set = train_datagen.flow_from_directory(train_path,
+
+training_set = train_datagen.flow_from_directory('imagens.nosync/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 class_mode = 'sparse')
 
-validation_set = validation_datagen.flow_from_directory('homer_bart.nosync/test_set',
+validation_set = validation_datagen.flow_from_directory('imagens.nosync/test_set',
                                                         target_size = (64, 64),
                                                         batch_size = 32,
-                                                        class_mode = 'binary')
+                                                        class_mode = 'sparse')
 
 
 # Executando o treinamento (esse processo pode levar bastante tempo, dependendo do seu computador)
-# classifier.fit_generator(training_set,
-#                          steps_per_epoch = 500,
-#                          epochs = 5)
-
-
 classifier.fit_generator(training_set,
-                         epochs = 10,
+                         epochs = 100,
                          validation_data = validation_set)
 
 
-# # Testes
-# labels = ["cat", "dog"]
-# hits = 0
-# errors = 0 
-
-# test_path = "dogs_cats.nosync/dataset_test/"
-
-# for label in labels:
-#     for n in range(500):
-#         test_image = image.load_img(f"{test_path}{label}.{n}.jpg", target_size = (64, 64))
-#         test_image = image.img_to_array(test_image)
-#         test_image = np.expand_dims(test_image, axis = 0)
-#         result = classifier.predict(test_image)
-#         training_set.class_indices
-
-#         if result[0][0] == 1:
-#             prediction = 'dog'
-#         else:
-#             prediction = 'cat'
-
-#         if(prediction == label):
-#             hits += 1
-#         else:
-#             errors += 1
-
-# print(f"From: {hits+errors}, {errors} errors and {hits} hits")
